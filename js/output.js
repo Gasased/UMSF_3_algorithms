1cfa43ae80a4ebe573fb891822fc21816f042fd2
Gasased <46537138+Gasased@users.noreply.github.com>
2022-11-21 02:40:52 +0200

    Move to python

--- js/output.js
+++
@@ -1,7 +0,0 @@
-function Output(n) {
-    document.querySelector('.output').innerHTML += `<div>${n}</div>`.replace(/,/g, ' ')
-}
-function OutputSteps(n) {
-    document.querySelector('.output').innerHTML += `<div style="color: #8e8e8e;">${n}</div>`.replace(/,/g, ' ')
-    // console.log(n);
-}
